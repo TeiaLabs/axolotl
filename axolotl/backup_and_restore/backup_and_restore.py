e3c0d10b69c8a98c0d1dfa3a01ed844286d54821
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-08-06 20:22:25 -0300

    Add cli to restore collection

--- axolotl/backup_and_restore/backup_and_restore.py
+++ axolotl/backup_and_restore/backup_and_restore.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 import typer
 from rich import print
@@ -14,6 +14,7 @@ def backup_collection(
     db: Annotated[str, Option("-db", "--database")],
     collection: Annotated[str, Option("-c", "--collection")],
     path: Annotated[str, Option("-p", "--path")],
+    offset: Annotated[int, Option("-o", "--offset")] = 0,
     dryrun: Annotated[bool, Option("-dryrun")] = False,
 ):
     try:
@@ -22,6 +23,7 @@ def backup_collection(
             collection=collection,
             path=path,
             dry_run=dryrun,
+            offset=offset,
         )
         if not dryrun:
             print(f"Backed up collection '{collection}' to '{path}'.")
@@ -32,8 +34,29 @@ def backup_collection(
 
 
 @app.command()
-def restore_collection():
-    pass
+def restore_collection(
+    db: Annotated[str, Option("-db", "--database")],
+    collection: Annotated[str, Option("-c", "--collection")],
+    path: Annotated[str, Option("-p", "--path")],
+    offset: Annotated[int, Option("-o", "--offset")] = 0,
+    limit: Annotated[int, Option("-l", "--limit")] = -1,
+    dryrun: Annotated[bool, Option("-dryrun")] = False,
+):
+    try:
+        BackupAndRestoreClient().restore_collection(
+            db=db,
+            collection=collection,
+            path=path,
+            offset=offset,
+            limit=limit,
+            dry_run=dryrun,
+        )
+        if not dryrun:
+            print(f"Restored collection '{collection}' to '{path}'.")
+        else:
+            print(f"Will Restore collection '{collection}' to '{path}'.")
+    except Exception as e:
+        print(e)
 
 
 @app.command()
