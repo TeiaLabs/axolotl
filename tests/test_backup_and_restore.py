99a9d446575bee4e620488a703ae3ec8a0c8d480
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2023-08-23 16:06:05 -0300

    Update test

--- tests/test_backup_and_restore.py
+++ tests/test_backup_and_restore.py
@@ -1,16 +1,48 @@
 import os
 import shutil
 from pathlib import Path
+from typing import Optional
 
 import dotenv
 import pytest
 from pymongo import MongoClient
-from redb.core import MongoConfig, RedB
-from teia_schema.instance import Instance
+from redb.core import Document, MongoConfig, RedB
 
 from axolotl.backup_and_restore.client import BackupAndRestoreClient
 
+
 dotenv.load_dotenv()
+
+
+class Dog(Document):
+    name: str
+    age: int
+    breed: str
+    color: str
+    is_good_boy: bool
+    is_pet: Optional[bool] = None
+
+    @classmethod
+    def get_hashable_fields(cls):
+        return [cls.name, cls.breed]
+
+    def __eq__(self, other: "Dog") -> bool:
+        if isinstance(other, Dog):
+            b = other.dict()
+        elif isinstance(other, dict):
+            b = other
+        else:
+            return False
+        a = self.dict()
+        a_keys = set(a.keys())
+        b_keys = set(b.keys())
+        assert a_keys == b_keys
+        # Iterating is better than just "=="
+        # in case of an error the log will be more readable :3
+        for a_key in a_keys:
+            if a_key != "updated_at":
+                assert a[a_key] == b[a_key]
+        return True
 
 
 class TestBackupAndRestore:
@@ -30,41 +62,43 @@ class TestBackupAndRestore:
     @pytest.fixture(autouse=True)
     def setup_instances(self):
         for i in range(5):
-            instance = Instance(
-                content=f"instance_{i}",
-                kb_name="test",
-                data_type="text",
+            instance = Dog(
+                name=f"instance_{i}",
+                age=i,
+                color="black",
+                breed="axolotl",
+                is_good_boy=True,
             )
             try:
                 instance.insert()
             except Exception as e:
                 print(e)
         yield
-        Instance.delete_many({})
+        Dog.delete_many({})
         shutil.rmtree(Path("./tmp/"))
 
     def test_backup_collection(self):
         self.backup_client.backup_collection(
             db="test_db_utils",
-            collection="instance",
+            collection="dog",
             path="./tmp/",
             dry_run=False,
         )
-        with open("./tmp/instance.jsonl") as f:
+        with open("./tmp/dog.jsonl") as f:
             assert len(f.readlines()) == 5
 
     def test_restore_collection(self):
         self.backup_client.backup_collection(
             db="test_db_utils",
-            collection="instance",
+            collection="dog",
             path="./tmp/",
             dry_run=False,
         )
-        Instance.delete_many({})
+        Dog.delete_many({})
         self.backup_client.restore_collection(
             db="test_db_utils",
-            collection="instance",
-            path="./tmp/instance.jsonl",
+            collection="dog",
+            path="./tmp/dog.jsonl",
             dry_run=False,
         )
-        assert len(Instance.find_many()) == 5
+        assert len(Dog.find_many()) == 5
